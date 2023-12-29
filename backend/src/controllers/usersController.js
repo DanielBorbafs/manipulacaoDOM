d0c78e609bde8ac66f59a719c605be24832b4336
DanielBorbafs <dev.danielborba@gmail.com>
2023-12-29 07:19:00 -0300

    Rota inicial configurada e validada

---
+++ backend/src/controllers/usersController.js
@@ -0,0 +1,10 @@
+
+
+const getAll = (_req, res) => {
+    return res.status(200).json ({ message: 'controller funcionando'});
+};
+
+
+module.exports = {
+    getAll
+}
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+require('dotenv').config();
+const PORT = process.env.PORT || 3333
+
+app.listen(3333, () => console.log(`Server rodando na porta ${PORT}`));
