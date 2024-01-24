870432cec9f54038bc310e0bbd2137a0ce85b69d
DanielBorbafs <dev.danielborba@gmail.com>
2024-01-24 14:18:02 -0300

    arquitetura de rotas definidas.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,3 @@
 const app = require('./app')
-require('dotenv').config();
-const PORT = process.env.PORT || 3333
 
-app.listen(3333, () => console.log(`Server rodando na porta ${PORT}`));
+app.listen(3333, () => console.log('Server rodando na porta 3333'));
