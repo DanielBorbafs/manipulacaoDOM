82be8469ca5b3bbc558a02eeeb4137562fe10bf4
DanielBorbafs <dev.danielborba@gmail.com>
2024-07-15 18:28:50 -0300

    fokus

---
+++ Fokus/script.js
@@ -0,0 +1,22 @@
+// BTNS
+const html = document.querySelector('html');
+const focoBt = document.getElementsByClassName('app__card-button--foco')[0];
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.getElementsByClassName('app__card-button--longo')[0];
+const banner = document.querySelector('.app__image')
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', './imagens/foco.png')
+})
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', './imagens/descanso-curto.png')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', './imagens/descanso-longo.png')
+})
+
